f5d3d50b4ba238c9ac91088cf3721db4638449b3
Ivan <vanchikalchuk@gmail.com>
2023-02-18 20:11:11 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+'user strict';
+
+// let category = 'toys';
+
+// console.log('https://somerurl.com/');
+
+
+// const answer = prompt('What is your name?', '');
+// const user = answer;
+// alert(`Hello, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+// console.log(5 % 2);
+
+console.log(2 + 4 * 2 !== '6');
+
+const isChecked = false,
+      isClose = false;
+console.log(isChecked || !isClose);
